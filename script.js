341bfca22f840d5c36c6f56fc5b45b5f8ee9a0ac
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-08-27 22:09:48 -0400

    Version 7
    Finally connecting JavaScript to HTML

---
+++ script.js
@@ -0,0 +1,80 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    // displayTodos should tell you if .todos is empty
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      // displayTodos should show .todoText
+      console.log('My todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        
+        // displayTodos should show .completed
+        if (this.todos[i].completed) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+        
+      } // end for loop
+    } // end else
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    
+    // Count completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed) {
+        completedTodos++;
+      }
+    }
+    
+    // Case 1: If everything's true, make everything false
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    // Case 2: Otherwise, make everything true
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    
+    this.displayTodos();
+  }
+};
+
+var displayTodosButton = document.getElementById('displayTodosButton'); // access displayTodosButton
+var toggleAllButton = document.getElementById('toggleAllButton'); // access toggleAllButton
+
+// displayTodos method when you click
+displayTodosButton.addEventListener('click', function () {
+  todoList.displayTodos();
+});
+
+// toggleAll method when you click the toggleAllButton
+toggleAllButton.addEventListener('click', function () {
+  todoList.toggleAll();
+});
