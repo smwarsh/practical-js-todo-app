aee68bb9db4a2ca5018e0d1a9235afbd41dd5334
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2018-09-03 12:25:32 -0400

    Version 10
    There is a delete button for each todo, and you can click the button to
    delete that todo. Got rid of the input/button delete field previously
    on the webpage

--- public/client.js
+++ public/client.js
@@ -56,10 +56,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -100,14 +98,21 @@ var view = {
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
     return deleteButton;
+  },
+  setUpEventListeners: function () {
+    var todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function (event) {
+      var elementClicked = event.target;
+
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
 };
 
 
-var todosUl = document.querySelector('ul');
-todosUl.addEventListener('click', function (event) {
-  console.log(event.target.parentNode.id);
-});
+view.setUpEventListeners();
 
 
 
